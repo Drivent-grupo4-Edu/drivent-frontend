160741c182f85436c5d49a6e5d684e2c4e028b55
Lorena Morais <lorenamorais@alunos.utfpr.edu.br>
2023-05-15 09:47:56 -0300

    feat: finished payment (not paid)

---
+++ src/services/paymentApi.js
@@ -0,0 +1,21 @@
+import api from './api';
+
+export async function save(body, token) {
+  const response = await api.post('payments/process', body, {
+    headers: {
+      Authorization: `Bearer ${token}`
+    }
+  });
+
+  return response.data;
+}
+
+export async function getPaymentsByTicketId(token) {
+  const response = await api.get('payments', {
+    headers: {
+      Authorization: `Bearer ${token}`
+    }
+  });
+
+  return response.data;
+}
