4dcc20c4fb709a33dce6aa70ad6ef4438b20f931
Leonardo Satoru Osugi <leoosugi96@gmail.com>
2023-05-18 17:33:28 -0300

    feat: NotAdress and FinishedTicket

--- src/contexts/ActiveTicketContext.js
+++ src/contexts/ActiveTicketContext.js
@@ -1,3 +1,11 @@
+import { useState } from 'react';
 import { createContext } from 'react';
 
 const ActiveTicketContext = createContext();
+export default ActiveTicketContext;
+
+export function ActiveTicketProvider({ children }) {
+  const [activeTicket, setActiveTicket] = useState(false);
+
+  return <ActiveTicketContext.Provider value={{ activeTicket, setActiveTicket }}>{ children }</ActiveTicketContext.Provider>;
+}
